fdb5f1c5119dac3e503fb8650a903cb88a969e2d
Olga <minaeva9@gmail.com>
2025-04-27 11:59:25 -0400

    use suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,22 +1,38 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import styles from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals()
+
+  return <MealsGrid meals={meals} />;
+  
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
-        <h1>Delicious meals, created <span className={styles.highlight}>by you</span></h1>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun.</p>
-        <p className={styles.cta}>
-          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
         </p>
-     </header>
-    <main className={styles.main}>
-      <MealsGrid meals={meals}/>
-    </main>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
     </>
   );
 }
